2e5784440e910d97d6110309c7ec6203ecd654bd
erik lima <erik.lima.f@outlook.com>
2022-09-05 01:03:33 -0300

    Adding deployment

--- src/pages/Skills/Components/Content/Card.styled.js
+++ src/pages/Skills/Components/Content/Card.styled.js
@@ -7,8 +7,10 @@ export const CardStyled = styled(Grid)`
   line-height: 15px;
   opacity: 100%;
   border-radius: 20px;
-  border: 1px solid #e0e0e0;
+  border: 1px solid #a0a0a0;
   display: inline-block;
+  box-shadow: -1px 1px 50px 0 rgba(0, 0, 0);
+
 
 
   h3 {
