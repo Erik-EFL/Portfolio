f2a11566983a4795a075c7a53e92246e69b4075f
erik lima <erik.lima.f@outlook.com>
2022-09-10 17:37:28 -0300

    feat(src/pages/projects): :sparkles: Criando dados
    Criação de um documento com informações dos projetos
    
    Adicionando as informações que devem ser consumidas pelos cards, como por exemplo, title, url e id

---
+++ src/pages/Projects/utils/Images.js
@@ -0,0 +1,66 @@
+import portfolio from './Image/portfolio.png';
+import queryBox from './Image/queryBox.png';
+import chefinho from './Image/chefinho.png';
+import starWarsSearch from './Image/logo-star-wars-750x375.jpg';
+import solarSystem from './Image/solarsystem.jpg';
+import wine from './Image/wine.png';
+
+export default [
+  {
+    id: 532724083,
+    url: 'https://github.com/Erik-EFL/Portifolio',
+    title: 'Portfolio',
+    img: portfolio,
+    description: 'Portfólio de meus trabalhos atuais e anteriores.',
+    technologies: ['React', 'Styled Components', 'MUI'],
+
+  },
+  {
+    id: 518212691,
+    url: 'https://github.com/Erik-EFL/Query-box-Game',
+    title: 'Query Box',
+    img: queryBox,
+    description: 'Query Box um jogo baseado na API de Perguntas Trivia',
+    technologies: ['React', 'Redux', 'CSS Module'],
+  },
+  {
+    id: 517488673,
+    url: 'https://github.com/Erik-EFL/recipe-app-Chefinho',
+    title: 'Chefinho Recipes',
+    img: chefinho,
+    description: 'Web app mobile first de receitas',
+    technologies: ['React', 'Styled Components', 'RTL'],
+  },
+  {
+    id: 517504676,
+    url: 'https://github.com/Erik-EFL/Solar-System',
+    title: 'Solar System',
+    img: solarSystem,
+    description: 'Web app do nosso sistema solar junto com curiosidades',
+    technologies: ['React', 'Jest', 'JavaScript'],
+  },
+  {
+    id: 517500584,
+    url: 'https://github.com/Erik-EFL/StarWars-Planets-Search',
+    title: 'StarWars Planets Search',
+    img: starWarsSearch,
+    description: 'Web app de planetas do StarWars junto com curiosidades sobre cada planeta',
+    technologies: ['React', 'Context API', 'JavaScript'],
+  },
+  {
+    id: 530811640,
+    url: 'https://github.com/Erik-EFL/Talker-Manager',
+    title: 'Talker Manager',
+    img: portfolio,
+    description: 'CRUD simples para criação de uma API REST de um sistema de palestras',
+    technologies: ['Node', 'JavaScript', 'JWT'],
+  },
+  {
+    id: 520568798,
+    url: 'https://github.com/Erik-EFL/wine-19-hackathon',
+    title: 'Clone Wine (Hackathon)',
+    img: wine,
+    description: 'Clone da Web page da Wine (Hackathon)',
+    technologies: ['React', 'Styled Components', 'MUI'],
+  },
+]
