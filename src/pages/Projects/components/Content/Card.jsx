c389b8a7fd026f47649f6d3ff378a8f42d6c18a0
erik lima <erik.lima.f@outlook.com>
2022-09-10 17:39:56 -0300

    feat(src/pages/projects): :rocket: Adicionando Cards
    Criando esqueleto dos cards
    
    esses cards são responsaveis pela apresentação dos projetos na pafina de projetos

--- src/pages/Projects/components/Content/Card.jsx
+++ src/pages/Projects/components/Content/Card.jsx
@@ -1,25 +1,36 @@
-import { Box, Container, Grid } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import React from 'react';
-import useFetch from '../../hooks/Fetch';
+import useFetchData from '../../hooks/FetchData';
 import {filtered} from '../../utils/Filtered';
 import { CardStyled } from './Card.styled';
+import data from '../../utils/Images'
 
 export default function Card() {
-
-  const data = useFetch();
-  const filteredData = filtered(data)
-
-  const projects = filteredData?.map((item, index) => (
-    <CardStyled key={index}>
-      <h3>{ item.name }</h3>
-      <p>{ item.description }</p>
-      <p>{ item.language }</p>
+  const projects = data?.map((item) => (
+    <CardStyled key={item.id}>
+      <div className="card-title">
+        <a href={item.url} target="_blank" title={item.title}><h3>{ item.title }</h3></a>
+      </div>
+      <div className='description'>
+        <p>{ item.description }</p>
+      </div>
+      <div className="containerImage">
+        <img src={item.img} alt="imagem de projeto" />
+      </div>
+      <div className='tech'>
+      {
+        item.technologies.map((tech) => (
+          <span>{tech}</span>
+        ))
+      }
+      </div>
     </CardStyled>
   ));
 
   return (
     <Container container component="main" sx={{
       maxWidth: '900px',
+      marginTop: '-30px',
     }}>
       <Grid container spacing={2} xs={12} sm={12} md={12} lg={12} sx={{
         gap: '30px',
@@ -28,7 +39,7 @@ export default function Card() {
         zIndex: '10',
         marginBottom: '120px',
       }}>
-       { projects}
+       { projects }
       </Grid>
     </Container>
   );
