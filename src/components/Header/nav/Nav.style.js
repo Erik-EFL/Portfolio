dce82aa763d3d2e9d98630e9d5e845a55b938921
erik lima <erik.lima.f@outlook.com>
2022-09-21 05:08:30 -0300

    feat(src/components/header): :sparkles: Crianção de novo component

--- src/components/Header/nav/Nav.style.js
+++ src/components/Header/nav/Nav.style.js
@@ -2,72 +2,89 @@ import styled from 'styled-components';
 import theme from '../../Themes/Themes'
 
 const NavMenu = styled.nav`
-  position: relative;
-  width: 350px;
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  padding: 0 25px;
-  margin-right: 56px;
-  align-items: center;
-  justify-content: center;
-  align-content: center;
-  grid-gap: 25px;
-
-  .icon {
-    margin-top: -10px;
-    font-size: 25px;
-    display: inline-flex;
+  @media (max-width: 900px) {
+    position: relative;
+    width: 200px;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    padding: 0 25px;
+    margin-right: 56px;
     align-items: center;
     justify-content: center;
-    text-decoration: none;
-    border-radius: 50%;
-    width: 40px;
-    height: 40px;
-    color: ${theme.defaults.text};
-    outline: 2px solid ${theme.defaults.text};
-    transition-property: outline-offset, outline-color, background-color;
-    transition-duration: 0.25s;
-    opacity: 1;
+    align-content: center;
+    grid-gap: 25px;
+
+    button {
+      border: none;
+      margin-bottom: 4px;
+      font-size: 15px;
+    }
+
+    .link {
+      display: block;
+      width: 100%;
+      height: 100%;
+      cursor: pointer;
+      opacity: 0.4;
+      text-decoration: none;
+      transition: all 0.5s ease-in-out;
+    }
+
+    .icon-social-media {
+      display: none;
+    }
+
+    .menu.active {
+      opacity: 1;
+      display: block;
+      position: absolute;
+      margin-top: 90px;
+      right: -24px;
+      transition: all 0.5s ease-in-out;
+      .icon-social-media {
+        display: flex;
+        transition: all 0.5s ease-in-out;
+      }
+    }
+
+    .menu {
+      opacity: 0;
+      display: none;
+    }
   }
 
-  .icon:hover {
-    outline-offset: 4px;
-  }
+  @media (min-width: 900px) {
+    width: 350px;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    padding: 0 25px;
+    margin-right: 56px;
+    align-items: center;
+    justify-content: center;
+    align-content: center;
+    grid-gap: 25px;
 
-  .icon:hover i{
-    animation: shake .25s;
-  }
 
-  .ri-instagram-line:hover {
-    background-image: radial-gradient(
-      circle at 30% 107%,
-      #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%
-    );
-    outline-color: #a02d76;
-  }
+    button {
+      display: none;
+    }
 
-  .ri-linkedin-fill:hover {
-    background-color: #0077b5;
-    outline-color: #0077b5;
-  }
+    .link {
+      width: 100%;
+      height: 100%;
+      cursor: pointer;
+      opacity: 0.4;
+      text-decoration: none;
+      transition: all 0.5s ease-in-out;
+    }
 
-  .ri-github-line:hover {
-    background-color: #000;
-    outline-color: #000;
-  }
+    .menu.active {
+      display: none;
+    }
 
-  .icons {
-    text-decoration: none;
-  }
-
-  .link {
-    display: block;
-    width: 100%;
-    height: 100%;
-    cursor: pointer;
-    opacity: 0.4;
-    text-decoration: none;
-    transition: all 0.5s ease-in-out;
+    .menu {
+      display: none;
+    }
   }
 
   .link:hover {
@@ -75,10 +92,12 @@ const NavMenu = styled.nav`
   }
 
   .link.active {
+    text-decoration: none;
     opacity: 1;
     transition: all 0.5s ease-in-out;
     color: ${theme.defaults.color};
   }
+
 `;
 
 export default NavMenu;
